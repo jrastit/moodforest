a61fca63cd6a4a982b2252b59376f9b1b438d3e0
Nityananda Gohain <nityanandagohain@gmail.com>
2019-10-26 21:18:19 +0530

    simple grid added with cards

--- src/js/app.js
+++ src/js/app.js
@@ -1,68 +1,97 @@
 App = {
-  web3Provider: null,
-  contracts: {},
+    web3Provider: null,
+    contracts: {},
 
-  init: async function() {
-    // Load pets.
-    $.getJSON('../pets.json', function(data) {
-      var petsRow = $('#petsRow');
-      var petTemplate = $('#petTemplate');
+    init: async function() {
+        // Load pets.
+        $.getJSON('../pets.json', function(data) {
+            var petsRow = $('#petsRow');
+            var petTemplate = $('#petTemplate');
 
-      for (i = 0; i < data.length; i ++) {
-        petTemplate.find('.panel-title').text(data[i].name);
-        petTemplate.find('img').attr('src', data[i].picture);
-        petTemplate.find('.pet-breed').text(data[i].breed);
-        petTemplate.find('.pet-age').text(data[i].age);
-        petTemplate.find('.pet-location').text(data[i].location);
-        petTemplate.find('.btn-adopt').attr('data-id', data[i].id);
+            for (i = 0; i < data.length; i++) {
 
-        petsRow.append(petTemplate.html());
-      }
-    });
+                petTemplate.find('.panel-title').text(data[i].animal);
+                // petTemplate.find('img').attr('src', data[i].picture);
+                petTemplate.find('.pet-breed').text(App.getColor(data[i].color));
+                petTemplate.find('.pet-age').text(App.getMood(data[i].mood));
+                petTemplate.find('.panel-body')[0].style.background = App.getColor(data[i].color);
+                petsRow.append(petTemplate.html());
+            }
+        });
 
-    return await App.initWeb3();
-  },
+        return await App.initWeb3();
+    },
 
-  initWeb3: async function() {
-    /*
-     * Replace me...
-     */
+    initWeb3: async function() {
+        /*
+         * Replace me...
+         */
 
-    return App.initContract();
-  },
+        return App.initContract();
+    },
 
-  initContract: function() {
-    /*
-     * Replace me...
-     */
+    initContract: function() {
+        /*
+         * Replace me...
+         */
 
-    return App.bindEvents();
-  },
+        return App.bindEvents();
+    },
 
-  bindEvents: function() {
-    $(document).on('click', '.btn-adopt', App.handleAdopt);
-  },
+    bindEvents: function() {
+        $(document).on('click', '.btn-adopt', App.handleAdopt);
+    },
 
-  markAdopted: function(adopters, account) {
-    /*
-     * Replace me...
-     */
-  },
+    markAdopted: function(adopters, account) {
+        /*
+         * Replace me...
+         */
+    },
 
-  handleAdopt: function(event) {
-    event.preventDefault();
+    handleAdopt: function(event) {
+        event.preventDefault();
 
-    var petId = parseInt($(event.target).data('id'));
+        var petId = parseInt($(event.target).data('id'));
 
-    /*
-     * Replace me...
-     */
-  }
+        /*
+         * Replace me...
+         */
+    },
+
+
+    getColor: function(color) {
+        switch (color) {
+            case 1:
+                return "Red"
+            case 2:
+                return "Blue"
+            case 3:
+                return "Green"
+            default:
+                return 0
+        }
+    },
+
+
+    getMood: function(mood) {
+        console.log("mood", mood)
+        switch (mood) {
+            case 1:
+                return "Happy"
+            case 2:
+                return "Sad"
+            case 3:
+                return "Embarassed"
+            default:
+                return 0
+        }
+    }
+
 
 };
 
 $(function() {
-  $(window).load(function() {
-    App.init();
-  });
+    $(window).load(function() {
+        App.init();
+    });
 });
